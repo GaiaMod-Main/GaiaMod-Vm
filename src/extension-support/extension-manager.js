af4810cd5cf37cda0a31c1af68764575a920ade4
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-06 17:36:21 -0400

    um

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -271,7 +271,7 @@ const defaultBuiltinExtensions = {
     typescratch: () => require('../extensions/typescratch'),
 	
 	// um...
-    //ScratchAlexa: () => require('../extensions/scratch3_alexa'),
+    TextToxicity: () => require('../extensions/scratch3_text_toxicity'),
 
     // e
     Scratch3WirelessBlocks: () => require('../extensions/scratch3_wireless'),
