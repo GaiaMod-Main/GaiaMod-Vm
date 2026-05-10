02fbb350ef148a738f977633887d232efb678444
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-10 08:45:55 -0400

    Update extension-manager.js

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -264,6 +264,7 @@ const defaultBuiltinExtensions = {
     croquet: () => require('../extensions/scratch3_croquet'),
     adahttp: () => require('../extensions/scratch3_adahttp'),
     chronometer: () => require('../extensions/adacraft_chronometer'),
+    adacloud: () => require('../extensions/adacraft_cloud'),
 	
     em_discordrpc: () => require('../extensions/em_discordrpc'),
     poweredUp: () => require('../extensions/scratch3_powered_up'),
