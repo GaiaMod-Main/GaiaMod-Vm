b90ea207c70f893e7bab6af2e4d865264eb26cfc
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-15 17:28:06 -0400

    Update extension-manager.js

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -287,7 +287,7 @@ const defaultBuiltinExtensions = {
 	//v
 	//mesh: () => require('../extensions/scratch3_mesh'),
     //meshV2: () => require('../extensions/scratch3_mesh_v2'),
-    smalrubotS1: () => require('../extensions/scratch3_smalrubot_s1'),
+    //smalrubotS1: () => require('../extensions/scratch3_smalrubot_s1'),
     //pr2Robot: () => require('../extensions/scratch3_pr2robot'),
     //fetchRobot: () => require('../extensions/scratch3_fetchrobot'),
     //spotRobot: () => require('../extensions/scratch3_spotrobot'),
