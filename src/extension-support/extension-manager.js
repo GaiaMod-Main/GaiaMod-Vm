4c99301ddd16dc1c7298a6ce5253cebc7f150399
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-25 02:22:52 -0400

    Update extension-manager.js

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -296,8 +296,8 @@ const defaultBuiltinExtensions = {
     beepboxsynth: () => require('../extensions/scratch3_beepbox_synth'),
     // Cognimates
     typescratch: () => require('../extensions/typescratch'),
-    alexa: () => require('../extensions/scratch3_alexa'),
-    //sentiment: () => require('../extensions/scratch3_sentiment'),
+    //alexa: () => require('../extensions/scratch3_alexa'),
+    sentiment: () => require('../extensions/scratch3_sentiment'),
     //cozmo: () => require('../extensions/scratch3_cozmo'),
     //hue: () => require('../extensions/scratch3_hue'),
     //wemo: () => require('../extensions/scratch3_wemo'),
