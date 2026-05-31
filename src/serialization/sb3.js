7ff3d8eaca5b1ea39460b6ddab62c08777cb6d72
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-05-31 04:22:33 -0400

    PLATFORM CHANGE TIME!

--- src/serialization/sb3.js
+++ src/serialization/sb3.js
@@ -919,8 +919,8 @@ const serialize = function (runtime, targetId, {allowOptimization = true} = {}) 
 
     // Attach platform information so TurboWarp and other mods can detect where the file comes from
     const platform = Object.create(null);
-    platform.name = "PenguinMod";
-    platform.url = "https://penguinmod.com/";
+    platform.name = "GaiaMod";
+    platform.url = "https://gaiamod-main.github.io/";
     platform.version = "stable";
     meta.platform = platform;
 
