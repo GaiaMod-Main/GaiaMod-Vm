755be9fe518543c7f3292057308f54ac6c9435ab
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-04 10:16:27 -0400

    Upload Code to GitHub

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -378,6 +378,13 @@ const defaultBuiltinExtensions = {
 	cubroidProximity: () => require('../extensions/scratch3_cubroid_proximity'),
 	echidna: () => require('../extensions/scratch3_echidna'),
 	eim: () => require('../extensions/scratch3_eim'),
+    em_discordrpc: () => require('../extensions/em_discordrpc'),
+    lepiBarcodeScan: () => require('../extensions/lepi_barcode_scan'),
+    lepiBalanceCar: () => require('../extensions/lepi_balance_car'),
+    homeassistant: () => require('../extensions/scratch3_homeassistant'),
+    lepiGoogleAI: () => require('../extensions/lepi_google_ai'),
+    lepiSmartClassroom: () => require('../extensions/lepi_smart_classroom'),
+
 	
 	// Custom extentions
     robot_communication: () => require('../extensions/scratch3_robot_communication'),
